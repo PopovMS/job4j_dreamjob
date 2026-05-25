097766ebe249838fde350ff92ba63b726c5e93dc
Misha <m.popov83@gmail.com>
2026-05-25 16:55:11 +0500

    Формы. Списки. [#504854]

--- src/main/java/ru/job4j/dreamjob/model/Vacancy.java
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -13,6 +13,8 @@ public class Vacancy {
 
     private boolean visible;
 
+    private int cityId;
+
     private final LocalDateTime creationDate = LocalDateTime.now()
             .withSecond(0)
             .withNano(0);
@@ -20,11 +22,12 @@ public class Vacancy {
     public Vacancy() {
     }
 
-    public Vacancy(int id, String title, String description, boolean visible) {
+    public Vacancy(int id, String title, String description, boolean visible, int cityId) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.visible = visible;
+        this.cityId = cityId;
     }
 
     public int getId() {
@@ -61,6 +64,14 @@ public class Vacancy {
 
     public void setVisible(boolean visible) {
         this.visible = visible;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
     }
 
     @Override
--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -20,12 +20,12 @@ public class MemoryVacancyRepository implements VacancyRepository {
 
 
     private MemoryVacancyRepository() {
-        save(new Vacancy(0, "Intern Java Developer", "Описание", false));
-        save(new Vacancy(0, "Junior Java Developer", "Описание", false));
-        save(new Vacancy(0, "Junior+ Java Developer", "Описание", false));
-        save(new Vacancy(0, "Middle Java Developer", "Описание", false));
-        save(new Vacancy(0, "Middle+ Java Developer", "Описание", false));
-        save(new Vacancy(0, "Senior Java Developer", "Описание", false));
+        save(new Vacancy(0, "Intern Java Developer", "Описание", false, 0));
+        save(new Vacancy(0, "Junior Java Developer", "Описание", false, 0));
+        save(new Vacancy(0, "Junior+ Java Developer", "Описание", false, 0));
+        save(new Vacancy(0, "Middle Java Developer", "Описание", false, 0));
+        save(new Vacancy(0, "Middle+ Java Developer", "Описание", false, 0));
+        save(new Vacancy(0, "Senior Java Developer", "Описание", false, 0));
     }
 
     @Override
@@ -43,7 +43,7 @@ public class MemoryVacancyRepository implements VacancyRepository {
     @Override
     public boolean update(Vacancy vacancy) {
         return vacancies.computeIfPresent(vacancy.getId(),
-                (id, oldVacancy) -> new Vacancy(oldVacancy.getId(), vacancy.getTitle(), vacancy.getDescription(), vacancy.getVisible())) != null;
+                (id, oldVacancy) -> new Vacancy(oldVacancy.getId(), vacancy.getTitle(), vacancy.getDescription(), vacancy.getVisible(), vacancy.getCityId())) != null;
     }
 
     @Override
